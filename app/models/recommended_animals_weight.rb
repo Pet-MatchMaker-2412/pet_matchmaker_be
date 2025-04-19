c3a313baeb994089e1aa2d4291b2f68c9d429ac3
Jaren Garman <jarengarman@protonmail.com>
2025-04-19 18:48:33 -0400

    feat: Add validation for weight being between 0 and 1 inclusively

--- app/models/recommended_animals_weight.rb
+++ app/models/recommended_animals_weight.rb
@@ -2,5 +2,5 @@ class RecommendedAnimalsWeight < ApplicationRecord
   belongs_to :recommended_animal
   belongs_to :answer
 
-  validates :weight, presence: true, numericality: true
+  validates :weight, presence: true, numericality: {in: 0..1}
 end
--- spec/models/recommended_animals_weight_spec.rb
+++ spec/models/recommended_animals_weight_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe RecommendedAnimalsWeight, type: :model do
 
   describe "Validations" do
     it { is_expected.to validate_presence_of :weight }
-    it { is_expected.to validate_numericality_of :weight }
+    it { is_expected.to validate_numericality_of(:weight).is_in(0..1) }
   end
 end
