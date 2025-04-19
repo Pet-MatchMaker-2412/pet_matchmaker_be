3e380c26f75fff45bd2a100a8d3d1758699b47c5
Jaren Garman <jarengarman@protonmail.com>
2025-04-18 22:25:28 -0400

    feat: Add username uniqueness validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :questionnaire_submissions
+
+  validates :username, presence: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  it { is_expected.to have_many :questionnaire_submissions }
+  describe "Relationships" do
+    it { is_expected.to have_many :questionnaire_submissions }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :username }
+    it { is_expected.to validate_uniqueness_of :username }
+  end
 end
