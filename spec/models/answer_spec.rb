1cc265aba20c18ac07a8729b6c6ef5f2e83daf9d
Jaren Garman <jarengarman@protonmail.com>
2025-04-18 22:11:38 -0400

    test: Add relationship tests for answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :question }
+  it { is_expected.to have_many :submission_answers }
+  it { is_expected.to have_many :recommended_animals_weights }
 end
