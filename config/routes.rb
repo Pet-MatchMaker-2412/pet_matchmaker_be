92773c7f77b3fd2652d6e53bfdc6aa2a45611ead
Jaren Garman <jarengarman@protonmail.com>
2025-04-20 16:04:42 -0400

    feat: Add route for get petfinder_animals

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: :create
+      resources :petfinder_animals, only: :index
     end
   end
 end
