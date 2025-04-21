2f8c670a6164856b1ec6f027e4ba24158565b722
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-21 08:23:28 -0700

    update: rendering json/adding rescue block

--- app/controllers/api/v1/questionnaire_submissions_controller.rb
+++ app/controllers/api/v1/questionnaire_submissions_controller.rb
@@ -1,11 +1,14 @@
 class Api::V1::QuestionnaireSubmissionsController < ApplicationController
   def index
-    begin
-    user = User.find(params[:user_id])
+    user = User.find_by(id: params[:user_id])
     
-    submissions = user.questionnaire_submissions.includes(:submission_answers, :recommended_animal)
-    submissions
-    rescue
+    if user
+      submissions = user.questionnaire_submissions.includes(:submission_answers, :recommended_animal)
+      render json: submissions, include: [:submission_answers, :recommended_animal]
+    else
+      render json: { error: "User not found" }, status: :not_found
     end
+  rescue => e
+    render json: { error: e.message }, status: :internal_server_error
   end
 end
