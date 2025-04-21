216af8dadc5b13cffa06e455013c50d8b76af2bd
Will Fox <william.macdonald.fox@gmail.com>
2025-04-20 21:45:18 -0400

    feat: customize QuestionSerializer to nest answers inside attributes block

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -2,5 +2,16 @@ class QuestionSerializer
   include JSONAPI::Serializer
 
   attributes :text
-  has_many :answers
+  
+  attribute :answers do |question|
+    question.answers.map do |answer|
+      {
+        id: answer.id.to_s,
+        type: "answer",
+        attributes: {
+          text: answer.text
+        }
+      }
+    end
+  end
 end
