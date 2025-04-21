d1e1d9cef9fc8e945cea41b97175a2807505595f
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-21 12:24:33 -0700

    chore: rebasing

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: :create
       resources :questions, only: :index
-        resources :questionnaire_submissions, only[:index] 
+        resources :questionnaire_submissions, only: :index 
     end
   end
 end
--- spec/requests/api/v1/users/questionnaire_submissions_request_spec.rb
+++ spec/requests/api/v1/users/questionnaire_submissions_request_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "GET /api/v1/users/:user_id/questionnaire_submissions", type: :request do
+  let(:user) { create(:user) }
+  require 'pry'; binding.pry
   describe "Happy Path" do
     context "can return users questionnaire submissions" do
-      post api_v1_users_questionnaire_submissions_path
+      get api_v1_users_questionnaire_submissions_path
     end
   end
 end
