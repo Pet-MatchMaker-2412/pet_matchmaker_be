1257d0696703eefc5cbe778c6882e9a140f3d7c7
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-21 08:07:53 -0700

    update: questionnaire_submissions spec successful response

--- spec/requests/api/v1/questionnaire_submissions_request_spec.rb
+++ spec/requests/api/v1/questionnaire_submissions_request_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "GET /api/v1/users/:user_id/questionnaire_submissions", type: :re
 
       it "returns a list of the user's questionnaire submissions" do
         get api_v1_user_questionnaire_submissions_path(user.id)
-require 'pry'; binding.pry
+
         expect(response).to be_successful
         expect(response.status).to eq(200)
       end
