c7bba1429a888476f196bfc9c568ba7771468dcd
Jaren Garman <jarengarman@protonmail.com>
2025-04-21 20:44:24 -0400

    refactor: Clean up serializers and remove trailing s

--- app/serializers/recommended_animal_serializer.rb
+++ app/serializers/recommended_animal_serializer.rb
@@ -1,6 +1,5 @@
 class RecommendedAnimalSerializer
   include JSONAPI::Serializer
 
-  set_type :recommended_animal
-  attributes :animal_type ##, :photo_url
+  attributes :animal_type
 end
--- app/serializers/submission_answer_serializer.rb
+++ app/serializers/submission_answer_serializer.rb
@@ -1,6 +1,5 @@
 class SubmissionAnswerSerializer
   include JSONAPI::Serializer
 
-  set_type :submission_answer
   attributes :answer_id
 end
