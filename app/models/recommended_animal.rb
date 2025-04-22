270150406b5ea7a3b2d0d4d36553ebb393a7b3c3
Will Fox <william.macdonald.fox@gmail.com>
2025-04-21 20:22:53 -0400

    feat: add validations for RecommendedAnimal model

--- app/models/recommended_animal.rb
+++ app/models/recommended_animal.rb
@@ -1,4 +1,7 @@
 class RecommendedAnimal < ApplicationRecord
   has_many :questionnaire_submissions
   has_many :recommended_animals_weights
+
+  validates :petf_type, presence: true
+  validates :photo_url, presence: true
 end
