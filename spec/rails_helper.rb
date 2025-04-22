98f46f6100ecd5d789cacebb854f395fcc96703f
Jaren Garman <jarengarman@protonmail.com>
2025-04-22 16:44:38 -0400

    feat: Add petfinder API key and secret to rails credentials

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,6 +83,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data("<Petfinder_API_KEY>") { Rails.application.credentials.petfinder[:key] }
+  config.filter_sensitive_data("<Petfinder_SECRET>") { Rails.application.credentials.petfinder[:secret] }
   config.default_cassette_options = {re_record_interval: 30.days}
   config.configure_rspec_metadata!
   config.before_record do |i|
