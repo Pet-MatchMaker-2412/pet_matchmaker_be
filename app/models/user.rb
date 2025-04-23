0fb44f68dd87e2aeb05821a4466398e14be623c4
Beverly Green <greenbev000@gmail.com>
2025-04-23 14:55:22 -0500

    added logic to find matching username with active record query method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   def self.find_by_user_by_username(json_request_body)
-
+    parsed_incoming_data = parse_json(json_request_body)
+    username = parsed_incoming_data["username"]
+    find_by(username: username)
   end
 
   def parse_json(json_request_body)
-    
+    JSON.parse(json_request_body)
   end
 end
