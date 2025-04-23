5992eb06f5d5dd33ded5d985105f0541bf1ef134
Jaren Garman <jarengarman@protonmail.com>
2025-04-23 16:05:17 -0400

    test: Add model test for add_answers

--- spec/models/questionnaire_submission_spec.rb
+++ spec/models/questionnaire_submission_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe QuestionnaireSubmission, type: :model do
     it { is_expected.to belong_to :recommended_animal }
     it { is_expected.to have_many :submission_answers }
   end
+
+  describe "Instance Methods" do
+    let(:submission) { described_class.new }
+
+    it "#add_answers" do
+      submission.add_answers(create_list(:answer, 10))
+
+      expect(submission.submission_answers.length).to eq(10)
+    end
+  end
 end
