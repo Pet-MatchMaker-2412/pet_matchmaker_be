597796756e3d30216539c7945381361332d1ea9e
Jaren Garman <jarengarman@protonmail.com>
2025-04-23 16:08:11 -0400

    feat: Add add_answers instance method for questionnaire_submissions

--- app/models/questionnaire_submission.rb
+++ app/models/questionnaire_submission.rb
@@ -2,4 +2,10 @@ class QuestionnaireSubmission < ApplicationRecord
   belongs_to :user
   belongs_to :recommended_animal
   has_many :submission_answers
+
+  def add_answers(answers)
+    answers.each do |answer|
+      submission_answers.new(answer: answer)
+    end
+  end
 end
