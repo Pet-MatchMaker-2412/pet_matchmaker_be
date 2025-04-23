7a0267b79b78af3e70919315557ab39c4b3d2bd3
Logan Sauer <ldsauer2@gmail.com>
2025-04-23 15:48:15 -0600

    added saved column to the questionnaire_submissions table with default value of false and wrote the tests for it.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_19_012344) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_23_194328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_19_012344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "recommended_animal_id", null: false
+    t.boolean "saved", default: false
     t.index ["recommended_animal_id"], name: "index_questionnaire_submissions_on_recommended_animal_id"
     t.index ["user_id"], name: "index_questionnaire_submissions_on_user_id"
   end
