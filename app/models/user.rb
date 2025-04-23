fe9c145c100653b7a0e5f6e8d0e7fce23b888f7e
Beverly Green <greenbev000@gmail.com>
2025-04-23 14:49:39 -0500

    started skeletons for model method to find username and helper method in model to parse json request body

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :questionnaire_submissions
 
   validates :username, presence: true, uniqueness: true
+
+  def self.find_by_user_by_username(json_request_body)
+
+  end
+
+  def parse_json(json_request_body)
+    
+  end
 end
