c9959cdca27c5b0b42f64ccf887f589a9484569a
Jaren Garman <jarengarman@protonmail.com>
2025-04-24 14:57:03 -0400

    refactor: Move submission_answers map outside iteration

--- app/models/recommended_animal.rb
+++ app/models/recommended_animal.rb
@@ -6,12 +6,12 @@ class RecommendedAnimal < ApplicationRecord
   validates :photo_url, presence: true
 
   def self.calculate_animal(submission_answers)
+    submission_answers_weights = submission_answers.map do |sub_answer|
+      sub_answer.recommended_animals_weights
+    end
+
     RecommendedAnimal.all.max_by do |animal|
       animal.recommended_animals_weights.sum do |db_weight|
-        submission_answers_weights = submission_answers.map do |sub_answer|
-          sub_answer.recommended_animals_weights
-        end
-
         if submission_answers_weights.any? { |weight_array| weight_array.include?(db_weight) }
           db_weight.weight
         else
