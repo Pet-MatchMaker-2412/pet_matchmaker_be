d488d5c421045e181f1c1ab0247e3a2131a79f64
Jaren Garman <jarengarman@protonmail.com>
2025-04-24 13:17:15 -0400

    feat: Add initial solution for the big kahuna

--- app/models/recommended_animal.rb
+++ app/models/recommended_animal.rb
@@ -4,4 +4,20 @@ class RecommendedAnimal < ApplicationRecord
 
   validates :petf_type, presence: true
   validates :photo_url, presence: true
+
+  def self.calculate_animal(submission_answers)
+    RecommendedAnimal.all.max_by do |animal|
+      animal.recommended_animals_weights.sum do |db_weight|
+        if submission_answers.map do |sub_answer|
+          sub_answer.recommended_animals_weights
+        end.any? do |weight_array|
+          weight_array.include?(db_weight)
+        end
+          db_weight.weight
+        else
+          0
+        end
+      end
+    end
+  end
 end
