86ec33a297719aa21c011f4e377f911efbfc78b7
Jaren Garman <jarengarman@protonmail.com>
2025-04-24 18:26:34 -0400

    chore: Add backedn api docs to CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173"
+    origins "localhost:5173", "https://pet-matchmaker-2412.github.io"
 
     resource "*",
       headers: :any,
