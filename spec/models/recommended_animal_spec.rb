d7efc304839f0d840c32bfe583b66dcd650e91e4
Jaren Garman <jarengarman@protonmail.com>
2025-04-24 13:14:24 -0400

    test: Add expectation to get cat as result

--- spec/models/recommended_animal_spec.rb
+++ spec/models/recommended_animal_spec.rb
@@ -17,14 +17,21 @@ RSpec.describe RecommendedAnimal, type: :model do
       DatabaseCleaner.clean_with(:truncation)
       load Rails.root.join("db/seeds.rb").to_s
 
-      submission = build(:questionnaire_submission, recommended_animal: nil)
-      submission_answers = [1, 4, 7, 10, 16, 19, 24, 25, 28, 32].map do |answer_id|
-        build(:submission_answer, questionnaire_submission: submission, answer: Answer.find(answer_id))
+      g_submission = build(:questionnaire_submission, recommended_animal: nil)
+      g_submission_answers = [1, 4, 7, 10, 16, 19, 24, 25, 28, 32].map do |answer_id|
+        build(:submission_answer, questionnaire_submission: g_submission, answer: Answer.find(answer_id))
+      end
+
+      c_submission = build(:questionnaire_submission, recommended_animal: nil)
+      c_submission_answers = [3, 6, 9, 10, 16, 20, 22, 25, 29, 33].map do |answer_id|
+        build(:submission_answer, questionnaire_submission: c_submission, answer: Answer.find(answer_id))
       end
 
       golden = described_class.find_by(animal_type: "Golden Retriever")
+      cat = described_class.find_by(animal_type: "Cat")
 
-      expect(described_class.calculate_animal(submission_answers)).to eq(golden)
+      expect(described_class.calculate_animal(g_submission_answers)).to eq(golden)
+      expect(described_class.calculate_animal(c_submission_answers)).to eq(cat)
     end
   end
 end
