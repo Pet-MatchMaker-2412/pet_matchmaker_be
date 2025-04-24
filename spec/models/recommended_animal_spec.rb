fe1931c8d632fc5c6aba6cd3c9073917914a17a7
Jaren Garman <jarengarman@protonmail.com>
2025-04-24 12:29:00 -0400

    test: Add test for calculate_animal class method

--- spec/models/recommended_animal_spec.rb
+++ spec/models/recommended_animal_spec.rb
@@ -6,9 +6,25 @@ RSpec.describe RecommendedAnimal, type: :model do
     it { is_expected.to have_many :recommended_animals_weights }
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:petf_type) }
-    it { should validate_presence_of(:photo_url) }
-    it { should_not validate_presence_of(:petf_breed) }
+  describe "Validations" do
+    it { is_expected.to validate_presence_of(:petf_type) }
+    it { is_expected.to validate_presence_of(:photo_url) }
+    it { is_expected.not_to validate_presence_of(:petf_breed) }
+  end
+
+  describe "Class Methods" do
+    it ".calculate_animal" do
+      DatabaseCleaner.clean_with(:truncation)
+      load Rails.root.join("db/seeds.rb").to_s
+
+      submission = create(:questionnaire_submission, recommended_animal: nil)
+      submission_answers = [1, 4, 7, 10, 16, 19, 24, 25, 28, 32].map do |answer_id|
+        create(:submission_answer, questionnaire_submission: submission, answer: Answer.find(answer_id))
+      end
+
+      golden = described_class.find_by(animal_type: "Golden Retriever")
+
+      expect(described_class.calculate_animal(submission_answers)).to eq(golden)
+    end
   end
 end
