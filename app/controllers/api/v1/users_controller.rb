ba33bb814048b539a87b536e86c23d62a37e312f
Jaren Garman <jarengarman@protonmail.com>
2025-04-23 21:45:25 -0400

    fix: Make index compatible with query params and remove unused instance method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.new(user_params)
+    user = User.new(username: user_params)
     if user.save
       render json: UserSerializer.new(user), status: :created
     else
@@ -8,13 +8,11 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  def index 
-    user = User.find_user_by_username(request.body.read)
-    if user == :missing_username
-      render json: ErrorSerializer.format_error(ErrorMessage.new("Username is required", 422)), status: :unprocessable_entity
-    elsif user == nil
+  def index
+    user = User.find_by(username: user_params)
+    if user.nil?
       render json: ErrorSerializer.format_error(ErrorMessage.new("User not found", 404)), status: :not_found
-    else 
+    else
       render json: UserSerializer.new(user), status: :ok
     end
   end
@@ -22,6 +20,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:username)
+    params.require(:username)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,4 @@ class User < ApplicationRecord
   has_many :questionnaire_submissions
 
   validates :username, presence: true, uniqueness: true
-
-  def self.find_user_by_username(json_request_body)
-    parsed_incoming_data = parse_json(json_request_body)
-    username = parsed_incoming_data["username"]
-
-    if username.blank?
-      return :missing_username
-    end
-
-    find_by(username: username)
-    rescue JSON::ParserError
-      :missing_username
-  end
-
-  private
-  def self.parse_json(json_request_body)
-    JSON.parse(json_request_body)
-  end
 end
